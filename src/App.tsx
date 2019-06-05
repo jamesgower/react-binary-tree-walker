fe81e7773e09b2a93fc13babe87b73b167c54756
James <james_gower@outlook.com>
2019-06-05 17:38:54 +0100

    added styling and functionality to tree walker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import "./App.css";
 import TreeVisualiser from "./components/TreeVisualiser";
 
 const App: React.FC = () => {
 	return (
-		<div>
-			<h1>Tree Walker</h1>
+		<>
 			<TreeVisualiser />
-		</div>
+		</>
 	);
 };
 
--- src/components/TreeVisualiser.tsx
+++ src/components/TreeVisualiser.tsx
@@ -1,67 +1,123 @@
-import React, { DOMElement } from "react";
+import React, { createRef } from "react";
 import Tree from "react-d3-tree";
 import BinarySearchTree from "../classes/BinarySearchTree";
-import TreeItem from "../classes/TreeItem";
 import { TreeState } from "../interfaces/ITreeVisualiser";
+import {
+	Input,
+	Button,
+	FormGroup,
+	Container,
+	FormText,
+	Col,
+	Row
+} from "reactstrap";
 
 const initialState: TreeState = {
 	tree: null,
 	treeType: null,
 	newNumber: "",
 	values: [],
-	translate: null
+	translate: {
+		x: null,
+		y: null
+	}
 };
 
-const containerStyles: React.CSSProperties = {
+const treeContainerStyles: React.CSSProperties = {
 	width: "100%",
-	height: "100vh"
+	height: "75vh",
+	border: "1px solid black"
 };
+
+const regex: RegExp = /[0-9]/g;
 
 class TreeVisualiser extends React.Component<object, TreeState> {
 	readonly state: TreeState = initialState;
-	public treeContainer: any;
+	private treeRef: React.RefObject<HTMLDivElement> = createRef();
 
 	componentDidMount() {
-		const dimensions = this.treeContainer.getBoundingClientRect();
+		const dimensions = this.treeRef.current.getBoundingClientRect();
 		this.setState({
 			translate: {
 				x: dimensions.width / 2,
-				y: dimensions.height / 4
+				y: 50
 			}
 		});
 	}
 	componentWillMount() {
-		const values: number[] = Array.from({ length: 5 }, () =>
-			Math.floor(Math.random() * 1000)
-		);
-		console.log(values);
-		const BST = new BinarySearchTree(values);
-		this.setState({ tree: BST.returnTree(), values });
+		this.createRandomTree(10, 1000);
 	}
 
 	render() {
+		const { newNumber, tree, translate } = this.state;
 		return (
-			<div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
-				{this.state.newNumber}
-				<Tree
-					data={this.state.tree && this.state.tree}
-					translate={this.state.translate}
-					orientation="vertical"
-				/>
-				<input
-					value={this.state.newNumber}
-					onChange={e => this.setState({ newNumber: e.target.value })}
-				/>
-				<button onClick={this.onAddNumber}>Add</button>
-			</div>
+			<Container>
+				<Row>
+					<Col md={3}>
+						<FormGroup>
+							<h4>Insert a node</h4>
+							<FormText>Add a node to the current tree:</FormText>
+							<Input
+								value={newNumber}
+								onChange={e =>
+									this.setState({
+										newNumber: e.target.value
+									})
+								}
+								style={{
+									display: "inline-flex",
+									width: "100px",
+									marginRight: "10px"
+								}}
+							/>
+							<Button
+								color="success"
+								style={{
+									display: "inline-flex",
+									marginTop: "-5px"
+								}}
+								onClick={this.onAddNumber}
+								disabled={!newNumber.match(regex)}
+							>
+								Add
+							</Button>
+						</FormGroup>
+					</Col>
+					<Col md={5}>
+						<h4>Set tree type</h4>
+						<Input type="select">
+							<option>Binary Search Tree</option>
+						</Input>
+					</Col>
+					<Col md={4}>
+						<h4>Begin tree traversal</h4>
+						<Input />
+					</Col>
+				</Row>
+				<div style={treeContainerStyles} ref={this.treeRef}>
+					<Tree
+						data={tree !== null && tree}
+						translate={translate}
+						orientation="vertical"
+					/>
+				</div>
+			</Container>
 		);
 	}
 
-	private onAddNumber = () => {
-		const values: number[] = this.state.values;
-		values.push(parseInt(this.state.newNumber));
-		const BST: BinarySearchTree = new BinarySearchTree(values);
-		this.setState({ tree: BST.returnTree(), values });
+	private onAddNumber = (): void => {
+		const { values, newNumber } = this.state;
+		values.push(parseInt(newNumber));
+		const BST = new BinarySearchTree(values);
+		return this.setState({ tree: BST.returnTree(), values, newNumber: "" });
+	};
+
+	private createRandomTree = (size: number, max: number): void => {
+		const values: number[] = Array.from({ length: size }, () =>
+			Math.floor(Math.random() * max)
+		);
+		const BST = new BinarySearchTree(values);
+		return this.setState({ tree: BST.returnTree(), values });
 	};
 }
 
